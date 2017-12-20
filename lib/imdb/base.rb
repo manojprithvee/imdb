bdb1d24031f3f2c0bc275f4e139beab8ec1beea4
Divakar Manoj <manoj.prithvee@gmail.com>
2017-12-20 19:22:04 +0530

    after bug fix

--- lib/imdb/base.rb
+++ lib/imdb/base.rb
@@ -14,45 +14,46 @@ module Imdb
     def initialize(imdb_id, title = nil)
       @id = imdb_id
       @url = "http://www.imdb.com/title/tt#{imdb_id}/combined"
-      @title = title.gsub(/"/, '').strip if title
+      @title = title.delete('"').strip if title
     end
-    
+
     # Returns an array with cast members
     def cast_members
-      document.search('table.cast td.nm a').map { |link| link.content.strip } rescue []
+      document.search('table.cast_list td[@class="itemprop"] a').map { |link| link.content.strip } rescue []
     end
 
     def cast_member_ids
-      document.search('table.cast td.nm a').map { |l| l['href'].sub(%r{^/name/(.*)/}, '\1') }
+      document.search('table.cast_list td[@class="itemprop"] a').map { |l| l['href'][/nm\d+/] }
     end
 
     # Returns an array with cast characters
     def cast_characters
-      document.search('table.cast td.char').map { |link| link.content.strip } rescue []
+      document.search('table.cast_list td[@class="character"]').map { |link| link.content.strip } rescue []
     end
 
     # Returns an Hash with cast members and characters
-    def cast_members_characters()
-      memb_char = Hash.new
+    def cast_members_characters
+      memb_char = {}
       cast_members.each_with_index do |_m, i|
-        memb_char[cast_members[i]]= cast_characters[i]
-      end
-      memb_char
-    end
-    # Returns an Hash with cast members id and characters
-    def cast_member_ids_characters()
-      memb_char = Hash.new
-      cast_member_ids.each_with_index do |_m, i|
-        memb_char[cast_member_ids[i]]= cast_characters[i]
+        memb_char[cast_members[i]] = cast_characters[i]
       end
       memb_char
     end
 
-    def cast_member_id_name_characters()
-      memb_char = Hash.new
-      man=cast_members_characters
+    # Returns an Hash with cast members id and characters
+    def cast_member_ids_characters
+      memb_char = {}
       cast_member_ids.each_with_index do |_m, i|
-        memb_char[cast_member_ids[i]]= [cast_members[i],cast_characters[i]]
+        memb_char[cast_member_ids[i]] = cast_characters[i]
+      end
+      memb_char
+    end
+
+    def cast_member_id_name_characters
+      memb_char = {}
+      man = cast_members_characters
+      cast_member_ids.each_with_index do |_m, i|
+        memb_char[cast_member_ids[i]] = [cast_members[i], cast_characters[i]]
       end
       memb_char
     end
@@ -63,14 +64,14 @@ module Imdb
     end
 
     def director_ids
-      document.search("h5[text()^='Director'] ~ div a").map { |link| link["href"].split("/")[2] } rescue []
+      document.search("h5[text()^='Director'] ~ div a").map { |link| link['href'].split('/')[2] } rescue []
     end
 
     def director_ids_hash
-      memb_char = Hash.new
-      man=cast_members_characters
+      memb_char = {}
+      man = cast_members_characters
       director_ids.each_with_index do |_m, i|
-        memb_char[_m]= [director[i]]
+        memb_char[_m] = [director[i]]
       end
       memb_char
     end
@@ -79,7 +80,7 @@ module Imdb
     def writers
       writers_list = []
 
-      fullcredits_document.search("h4[text()^='Writing Credits'] + table tbody tr td[class='name']").each_with_index do |name, i|
+      fullcredits_document.search("h4[text()^='Writing Credits'] + table tbody tr td[class='name']").each_with_index do |name, _i|
         writers_list << name.content.strip unless writers_list.include? name.content.strip
       end rescue []
 
@@ -90,111 +91,98 @@ module Imdb
     def writers_ids
       writers_list = []
 
-      fullcredits_document.search("h4[text()^='Writing Credits'] + table tbody tr td[class='name'] a").each_with_index do |name, i|
-        writers_list << name['href'].split("/")[2] unless writers_list.include? name['href'].split("/")[2]
+      fullcredits_document.search("h4[text()^='Writing Credits'] + table tbody tr td[class='name'] a").each_with_index do |name, _i|
+        writers_list << name['href'].split('/')[2] unless writers_list.include? name['href'].split('/')[2]
       end rescue []
 
       writers_list.uniq
     end
 
     def award_name
-      awards_document.xpath("//div[@id='content-2-wide']/div[@id='main']/div/div[@class='article listo']/h3").map{|a|a.text.strip} rescue []
+      awards_document.xpath("//div[@id='content-2-wide']/div[@id='main']/div/div[@class='article listo']/h3").map { |a| a.text.strip } rescue []
     end
 
     def awards_table
-      awards=Hash.new
-      award_name.each{|movie|
-      movietest=movie[/.*[^A-Za-z ]/]
-      movietest=movietest.split('"')[0]
-      movie1=movie.gsub("\n","")
-      temp=Hash.new
-      
-      # puts movietest
-      abc=awards_document.search("h3:contains(\"#{movietest}\") ~ table")[0]
-      # puts movie,abc
-      list=abc.search("td @rowspan").map{|a| a.text}
-      list1=abc.search("td[@rowspan]").map{|a| a.text.gsub("\n","").gsub('"',"").strip.split(/\s{2,}/)}
-      abcd=abc.search("td:last-child")
-      listwon=abcd[0...list[0].to_i] if list.size>=1
-      listnom=abcd[list[0].to_i...list[0].to_i+list[1].to_i] if list.size>=2
-      list2nd=abcd[list[0].to_i+list[1].to_i...list[0].to_i+list[1].to_i+list[2].to_i] if list.size>=3
-      list3rd=abcd[list[0].to_i+list[1].to_i+list[2].to_i...list[0].to_i+list[1].to_i+list[2].to_i+list[3].to_i] if list.size>=4
-      list4th=abcd[list[0].to_i+list[1].to_i+list[2].to_i+list[3].to_i...list[0].to_i+list[1].to_i+list[2].to_i+list[3].to_i+list[4].to_i] if list.size>=5
-      listwon.each{|a|
-        name=a.at("text()").text.strip
-        to=a.search("a @href").map{|a| a.text.split("?")[0].gsub("/name/","") if !a.text.include? "title" and !a.text.include? "company" and !a.text.include? "javascript" }.compact
-        extra=a.search("div text()").text.strip
-          if (!to.nil? and !to.empty?) or (!name.nil? and name.present?)
-            if temp[list1[0]].nil?
-              temp[list1[0]]=Array.new
-            end
-            temp[list1[0]]+=[[name,to,extra]]
+      awards = {}
+      award_name.each do |movie|
+        movietest = movie[/.*[^A-Za-z ]/]
+        movietest = movietest.split('"')[0]
+        movie1 = movie.delete("\n")
+        temp = {}
+
+        # puts movietest
+        abc = awards_document.search("h3:contains(\"#{movietest}\") ~ table")[0]
+        # puts movie,abc
+        list = abc.search('td @rowspan').map(&:text)
+        list1 = abc.search('td[@rowspan]').map { |a| a.text.delete("\n").delete('"').strip.split(/\s{2,}/) }
+        abcd = abc.search('td:last-child')
+        listwon = abcd[0...list[0].to_i] if list.size >= 1
+        listnom = abcd[list[0].to_i...list[0].to_i + list[1].to_i] if list.size >= 2
+        list2nd = abcd[list[0].to_i + list[1].to_i...list[0].to_i + list[1].to_i + list[2].to_i] if list.size >= 3
+        list3rd = abcd[list[0].to_i + list[1].to_i + list[2].to_i...list[0].to_i + list[1].to_i + list[2].to_i + list[3].to_i] if list.size >= 4
+        list4th = abcd[list[0].to_i + list[1].to_i + list[2].to_i + list[3].to_i...list[0].to_i + list[1].to_i + list[2].to_i + list[3].to_i + list[4].to_i] if list.size >= 5
+        listwon.each do |a|
+          name = a.at('text()').text.strip
+          to = a.search('a @href').map { |a| a.text.split('?')[0].gsub('/name/', '') if !a.text.include?('title') && !a.text.include?('company') && !a.text.include?('javascript') }.compact
+          extra = a.search('div text()').text.strip
+          if (!to.nil? && !to.empty?) || (!name.nil? && name.present?)
+            temp[list1[0]] = [] if temp[list1[0]].nil?
+            temp[list1[0]] += [[name, to, extra]]
           end
-      }
-      if !list1[1].nil?
-        listnom.each{|a|
-          name=a.at("text()").text.strip
-          to=a.search("a @href").map{|a| a.text.split("?")[0].gsub("/name/","") if !a.text.include? "title" and !a.text.include? "company" and !a.text.include? "javascript" }.compact
-          extra=a.search("div text()").text.strip
-          if (!to.nil? and !to.empty?) or (!name.nil? and name.present?)
-            if temp[list1[1]].nil?
-              temp[list1[1]]=Array.new
+        end
+        unless list1[1].nil?
+          listnom.each do |a|
+            name = a.at('text()').text.strip
+            to = a.search('a @href').map { |a| a.text.split('?')[0].gsub('/name/', '') if !a.text.include?('title') && !a.text.include?('company') && !a.text.include?('javascript') }.compact
+            extra = a.search('div text()').text.strip
+            if (!to.nil? && !to.empty?) || (!name.nil? && name.present?)
+              temp[list1[1]] = [] if temp[list1[1]].nil?
+              temp[list1[1]] += [[name, to, extra]]
             end
-            temp[list1[1]]+=[[name,to,extra]]
           end
-        }
-      end
-      if !list1[2].nil?
-        list2nd.each{|a|
-          name=a.at("text()").text.strip
-          to=a.search("a @href").map{|a| a.text.split("?")[0].gsub("/name/","") if !a.text.include? "title" and !a.text.include? "company" and !a.text.include? "javascript" }.compact
-          extra=a.search("div text()").text.strip
-          if (!to.nil? and !to.empty?) or (!name.nil? and name.present?)
-            if temp[list1[2]].nil?
-              temp[list1[2]]=Array.new
+        end
+        unless list1[2].nil?
+          list2nd.each do |a|
+            name = a.at('text()').text.strip
+            to = a.search('a @href').map { |a| a.text.split('?')[0].gsub('/name/', '') if !a.text.include?('title') && !a.text.include?('company') && !a.text.include?('javascript') }.compact
+            extra = a.search('div text()').text.strip
+            if (!to.nil? && !to.empty?) || (!name.nil? && name.present?)
+              temp[list1[2]] = [] if temp[list1[2]].nil?
+              temp[list1[2]] += [[name, to, extra]]
             end
-            temp[list1[2]]+=[[name,to,extra]]
           end
-        }
-      end
-      if !list1[3].nil?
-         list3rd.each{|a|
-          name=a.at("text()").text.strip
-          to=a.search("a @href").map{|a| a.text.split("?")[0].gsub("/name/","") if !a.text.include? "title" and !a.text.include? "company" and !a.text.include? "javascript" }.compact
-          extra=a.search("div text()").text.strip
-          if (!to.nil? and !to.empty?) or (!name.nil? and name.present?)
-            if temp[list1[3]].nil?
-              temp[list1[3]]=Array.new
+        end
+        unless list1[3].nil?
+          list3rd.each do |a|
+            name = a.at('text()').text.strip
+            to = a.search('a @href').map { |a| a.text.split('?')[0].gsub('/name/', '') if !a.text.include?('title') && !a.text.include?('company') && !a.text.include?('javascript') }.compact
+            extra = a.search('div text()').text.strip
+            if (!to.nil? && !to.empty?) || (!name.nil? && name.present?)
+              temp[list1[3]] = [] if temp[list1[3]].nil?
+              temp[list1[3]] += [[name, to, extra]]
             end
-            temp[list1[3]]+=[[name,to,extra]]
           end
-        }
-      end
-      if !list1[4].nil?
-         list4th.each{|a|
-          name=a.at("text()").text.strip
-          to=a.search("a @href").map{|a| a.text.split("?")[0].gsub("/name/","") if !a.text.include? "title" and !a.text.include? "company" and !a.text.include? "javascript" }.compact
-          extra=a.search("div text()").text.strip
-          if (!to.nil? and !to.empty?) or (!name.nil? and name.present?)
-            
-            if temp[list1[4]].nil?
-              temp[list1[4]]=Array.new
-            end
-            temp[list1[4]]+=[[name,to,extra]]
+        end
+        unless list1[4].nil?
+          list4th.each do |a|
+            name = a.at('text()').text.strip
+            to = a.search('a @href').map { |a| a.text.split('?')[0].gsub('/name/', '') if !a.text.include?('title') && !a.text.include?('company') && !a.text.include?('javascript') }.compact
+            extra = a.search('div text()').text.strip
+            next unless (!to.nil? && !to.empty?) || (!name.nil? && name.present?)
+
+            temp[list1[4]] = [] if temp[list1[4]].nil?
+            temp[list1[4]] += [[name, to, extra]]
           end
-        }
+        end
+        awards[movie1] = temp unless temp.empty?
       end
-      if !temp.empty?
-        awards[movie1]=temp
-      end
-    }
-    return awards
+      awards
     end
 
     def writers_ids_hash
-      memb_char = Hash.new
+      memb_char = {}
       writers_ids.each_with_index do |_m, i|
-        memb_char[_m]= [writers[i]]
+        memb_char[_m] = [writers[i]]
       end
       memb_char
     end
@@ -206,22 +194,22 @@ module Imdb
 
     # Returns an array of genres (as strings)
     def genres
-      document.search("h5[text()='Genre:'] ~ div a[@href*='/Sections/Genres/']").map { |link| link.content.strip } rescue []
+      document.search("td[text()='Genres'] ~ td ul li a").map { |link| link.content.strip } rescue []
     end
 
     # Returns an array of languages as strings.
     def languages
-      document.search("li a[@href*='/language/']").map { |link| link.content.strip } rescue []
+      document.search("td[text()='Language'] ~ td ul li a").map { |link| link.content.strip } rescue []
     end
 
     # Returns an array of countries as strings.
     def countries
-      document.search("h5[text()='Country:'] ~ div a[@href*='/country/']").map { |link| link.content.strip } rescue []
+      document.search("td[text()='Country'] ~ td a[@href*='/country/']").map { |link| link.content.strip } rescue []
     end
 
     # Returns the duration of the movie in minutes as an integer.
     def length
-      document.at("h5[text()='Runtime:'] ~ div").content[/\d+ min/].to_i rescue nil
+      document.search("td[text()='Runtime'] ~ td ul li").text[/\d+ min/].to_i rescue nil
     end
 
     # Returns the company
@@ -246,12 +234,12 @@ module Imdb
     end
 
     def awards
-      document.search("h5[text()='Awards:'] ~ div").map { |link| link.content.strip.gsub("\nSee more »","").gsub("\n"," ") } rescue nil
+      document.search("h5[text()='Awards:'] ~ div").map { |link| link.content.strip.gsub("\nSee more »", '').tr("\n", ' ') } rescue nil
     end
 
     # Returns a string containing the URL to the movie poster.
     def poster
-      src = document.at("a[@name='poster'] img")['src'] rescue nil
+      src = document.at('.subpage_title_block a img')['src'] rescue nil
       case src
       when /^(https:.+@@)/
         Regexp.last_match[1] + '.jpg'
@@ -264,7 +252,7 @@ module Imdb
     def rating
       document.at('.ipl-rating-star__rating').content.split('/').first.strip.to_f rescue nil
     end
-    
+
     # Returns an int containing the Metascore
     def metascore
       criticreviews_document.at('//span[@itemprop="ratingValue"]').content.to_i rescue nil
@@ -272,7 +260,7 @@ module Imdb
 
     # Returns an int containing the number of user ratings
     def votes
-      Imdb::Search.new("secret superstar (2017)").movies.first.document.at('.ipl-rating-star__total-votes').content.strip.gsub(/[^\d+]/, '').to_i rescue nil
+      document.at('.ipl-rating-star__total-votes').content.strip.gsub(/[^\d+]/, '').to_i rescue nil
     end
 
     # Returns a string containing the tagline
@@ -295,16 +283,17 @@ module Imdb
     end
 
     def type
-      return (document.search("span[class='title-extra']").text.include?("TV series") or document.search("div[id='tn15epnav']").text.include?("Episodes"))
-    end 
+      (document.search("span[class='title-extra']").text.include?('TV series') || document.search("div[id='tn15epnav']").text.include?('Episodes'))
+    end
+
     # Returns an integer containing the year (CCYY) the movie was released in.
     def year
-      document.at("h3[@itemprop='name'] span").content.to_i rescue nil
+      document.at("h3[@itemprop='name'] span").content[/\d+/].to_i rescue nil
     end
 
     # Returns release date for the movie.
     def release_date
-      sanitize_release_date(document.at("h5[text()*='Release Date'] ~ div").content) rescue nil
+      sanitize_release_date(document.at("a[@href*='releaseinfo']").content[/\d{2}\s\S{3}\s\d{4}/]) rescue nil
     end
 
     # Returns filming locations from imdb_url/locations
@@ -321,6 +310,7 @@ module Imdb
         }
       end rescue []
     end
+
     # Returns a new Nokogiri document for parsing.
     def document
       @document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id))
@@ -337,7 +327,7 @@ module Imdb
     def fullcredits_document
       @fullcredits_document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id, 'fullcredits'))
     end
-    
+
     def criticreviews_document
       @criticreviews_document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id, 'criticreviews'))
     end
@@ -345,16 +335,16 @@ module Imdb
     def awards_document
       @awards_document ||= Nokogiri::HTML(Imdb::Movie.find_by_id(@id, 'awards'))
     end
-    
+
     # Use HTTParty to fetch the raw HTML for this movie.
     def self.find_by_id(imdb_id, page = :combined)
-      try=0
+      try = 0
       begin
-      open("http://www.imdb.com/title/tt#{imdb_id}/#{page}")
+        open("http://www.imdb.com/title/tt#{imdb_id}/#{page}")
       rescue
-        if try<=3
-          puts "aaaaaa"
-          try+=1
+        if try <= 3
+          puts 'aaaaaa'
+          try += 1
           sleep(2)
           retry
         end
